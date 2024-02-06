5f6dbcb575e786763b2a573476842b610e50cab5
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-02-06 09:37:11 +0000

    Start working

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req,res) => {
+
+})
+
+app.get("/create-redirect", (req, res) => {
+    const url = req.query.url
+
+    
+})
+
+app.listen(3000, () => {
+
+})
